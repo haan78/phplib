71288cf2439ee3b062f79cd20f7e54a72962d664
haan78 <alibarisozturk@gmail.com>
2019-12-10 08:37:19 +0200

    Bu ilk commit

--- JsPage.php
+++ JsPage.php
@@ -1,38 +1,60 @@
 <?php
+
 class jsPage {
+
     public static $title;
-    public static $loading;
     public static $div_app_id = "app";
     public static $div_loading_id = "__loadig__";
     public static $window_data_element_name = "data";
     public static $favicon = "assets/img/favicon.ico";
-    public static function load($file,$data) {
-        ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title><?php echo self::$title; ?></title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="icon" href="<?php echo self::$favicon; ?>" type="image/x-icon" />
-</head>
-<body>
-    <div id="<?php echo self::$div_loading_id; ?>"><b><?php echo self::$loading; ?></b></div>
-    <div id="<?php echo self::$div_app_id; ?>"></div>
-    <script>        
-        <?php
-        echo "window.".self::$window_data_element_name." = {};";
+
+    public function __construct($file, $data = null) {
+        $this->load($file, $data);
+    }
+
+    protected function putLoading() {
+        ?><b>Please Wait...</b><?php
+    }
+
+    protected function putData($data) {
         if (is_array($data)) {
+            echo "window." . self::$window_data_element_name . " = {};";
             foreach ($data as $k => $v) {
-                echo "window.data." . $k . "=" . json_encode($v) . ";" . PHP_EOL;
+                echo "window." . self::$window_data_element_name . ".['" . $k . "']=" . json_encode($v) . ";" . PHP_EOL;
             }
         }
-        echo file_get_contents($file);
-        ?>
-        document.getElementById("<?php echo self::$div_loading_id; ?>").style.display = "none";
-    </script>
-</body>
-</html> <?php
     }
+
+    protected function putFile($file) {
+        if (file_exists($file)) {
+            echo file_get_contents($file);
+        } else {
+            echo "File $file not found";
+        }
+    }
+
+    public function load($file, $data = null) {
+        ?><!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset='utf-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <title><?php echo self::$title; ?></title>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                <link rel="icon" href="<?php echo self::$favicon; ?>" type="image/x-icon" />
+            </head>
+            <body>
+                <div id="<?php echo self::$div_loading_id; ?>"><?php $this->putLoading() ?></div>
+                <div id="<?php echo self::$div_app_id; ?>"></div>
+                <script>
+        <?php
+        $this->putData($data);
+        $this->putFile($file);
+        ?>
+                    document.getElementById("<?php echo self::$div_loading_id; ?>").style.display = "none";
+                </script>
+            </body>
+        </html><?php
+    }
+
 }
